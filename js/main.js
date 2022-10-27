fb6f133e6b53791204d26255cc06be1fd59b0a98
SimoVisc <simonaviscardivs@gmail.com>
2022-10-27 20:10:49 +0200

    aggiunge elementi della lista ma c'è un errore

--- js/main.js
+++ js/main.js
@@ -1,12 +1,16 @@
 "use strict"
 // creo la lista della spesa
-const shoppingList=["pane","pomodori","tonno","uova", "latte"]
+const shoppingList=[
+    "pane",
+    "pomodori",
+    "tonno",
+    "uova", 
+    "latte"
+]
 // per ogni elemento della lista creo un elemento <li>
 let i=0
-const listContainer= document.getElementById("list")
+const Container= document.getElementById("list")
 while(i<shoppingList.length){
-    const listItem= document.createElement("li");
-    listItem.innerHTML=shoppingList[i]
-    listContainer.append(listItem)
-
+    Container.innerHTML+= ` <li>${shoppingList[i]}</li>`
 }
+console.log(shoppingList[i],  Container )
