6f068bb7c0187611764ff077ccb916723f3e4ca8
SimoVisc <simonaviscardivs@gmail.com>
2022-10-27 14:56:04 +0200

    aggiunge il ciclo for

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
 "use strict"
 // creo la lista della spesa
 const list=["pane","pomodori","tonno","uova", "latte"]
+// per ogni elemento della lista creo un elemento <li>
+for(let i=0; i< list.length; i++){
+    console.log(list[i]);
+}
